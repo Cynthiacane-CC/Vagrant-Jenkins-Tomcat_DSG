b0227fdda44e3c8a6bec3d09a71b9761ba102353
Cynthiacane-CC <cynthiacane@yahoo.fr>
2021-11-04 20:06:55 -0400

    pushing vagrantfile Jenkins and tomcat to github

--- tomcat/tomcat.sh
+++ tomcat/tomcat.sh
@@ -1,11 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 
 
 #Script to setup Apache Tomcat on centOS/RHEL 6.x and 7.x
 
 #Author Serge  Aug 2017
 #Modify: sept 2019
-#Modified sept 2021
 
 
 echo -e "\nInstalling tomcat ..\n"
@@ -13,8 +12,6 @@ echo -e "\nInstalling tomcat ..\n"
 sleep 4
 
 cd /opt
-rm -rf /opt/apache-tomcat-*
-yum install java-1.8* wget vim epel-release -y
 
 wget http://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-8/v8.5.71/bin/apache-tomcat-8.5.71.tar.gz
 tar -xzvf apache-tomcat-8.5.71.tar.gz
@@ -25,26 +22,13 @@ mv apache* tomcat
 chmod +x /opt/tomcat/bin/startup.sh
 chmod +x /opt/tomcat/bin/shutdown.sh
 
-if  [ -f /usr/local/bin/tomcatup ] 
-then
-rm -rf  /usr/local/bin/tomcatup
- ln -s /opt/tomcat/bin/startup.sh /usr/local/bin/tomcatup
- elif
- [ -f /usr/local/bin/tomcatup ] 
-then
-rm -rf  /usr/local/bin/tomcatdown
- ln -s /opt/tomcat/bin/shutdown.sh /usr/local/bin/tomcatdown
-fi
+ln -s /opt/tomcat/bin/startup.sh /usr/local/bin/tomcatup
+ln -s /opt/tomcat/bin/shutdown.sh /usr/local/bin/tomcatdown
 
 echo -e "\nStarting tomcat\n"
 sleep 4
 
 tomcatup
-
-if [ ${?} -ne 0 ]
-then
-/opt/tomcat/bin/startup.sh
-fi
 
 echo -e "\n Installing ip tables package...\n"
 
